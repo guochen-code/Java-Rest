98f7fc1ba4cd22dd33efce604bd2d73694ed30f0
guochen-code <83231868+guochen-code@users.noreply.github.com>
2024-02-23 15:09:49 -0700

    Create example.java

---
+++ Spring/example.java
@@ -0,0 +1,57 @@
+//script-001 - main
+@Configuration
+@ComponentScan
+public class ApplicationRunner {
+
+	public static void main(String[] args) {
+		try(var context =
+				new AnnotationConfigApplicationContext (ApplicationRunner.class)){
+			
+			Arrays.stream(context.getBeanDefinitionNames())
+			.forEach(System.out::println);
+			
+			System.out.println(context.getBean(BusinessCalculationService.class).findMax());
+		}
+		
+	}
+
+}
+
+//script-002 - interface
+public interface DataService {
+	
+	int[] retrieveData();
+
+}
+
+//script-003 - interface implementation-001
+@Component
+@Primary
+public class MySQLDataService implements DataService{
+	
+	public int[] retrieveData() {
+		return new int[] {1,2,3,4,5};
+	}
+}
+
+//script-004 - interface implementation-002
+@Component
+public class MongoDbDataService implements DataService{
+	
+	public int[] retrieveData() {
+		return new int[] {11,22,33,44,55};
+	}
+}
+
+
+//script-005 - 
+@Component
+public class BusinessCalculationService {
+	
+	@Autowired
+	private DataService dataservice;
+	
+	public int findMax() {
+		return Arrays.stream(dataservice.retrieveData()).max().orElse(0);
+	}
+}
