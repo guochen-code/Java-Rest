beb7d3217a8d98ebb82f19bf35899b83aed7f6a0
guochen-code <83231868+guochen-code@users.noreply.github.com>
2024-02-23 21:25:13 -0700

    Update static.java

--- static.java
+++ static.java
@@ -15,3 +15,7 @@ static public int getCount() {
 // count is shared variable across all instances of this class.
 // you can call static method directly using class object.
 // static method can also be called from instance, not recommended.
+// in static method, cannot access non-static variable such as name, and also cannot call non-static method
+
+// static means it's a class variable
+// final means this value cannot change
