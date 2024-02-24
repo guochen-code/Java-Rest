7c161b2fd85d7e3105b4a172d2cefc2c707182b1
guochen-code <83231868+guochen-code@users.noreply.github.com>
2024-02-23 21:47:30 -0700

    Update enum.java

--- enum.java
+++ enum.java
@@ -1,8 +1,29 @@
 enum Season {
-  Summer, Winter, Fall, Spring'
+  Summer, Winter, Fall, Spring;
 }
 
 Season season = Season.Fall;
 Season.Spring.ordinal();
 Arrays.toString(Season.values());
 
+// ordinal changes along with position
+// how to give a fixed id:
+
+enum Season {
+  Summer(1), Winter(2), Fall(3), Spring(4);
+
+  private int value;
+
+  private Season(int value) { ///////////// enum can have a constructor
+    this.value=value;
+  }
+
+  public int getValue() { //////////////// enum can have method
+    return value
+      }
+}
+
+Season.Spring.getValue();
+
+// enum can be in a separate script:
+public enum Season {}
